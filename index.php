b1fda7ca936d24643efcffd3aa77ea0c1289d736
Jock Perkins <jockperkins@jocks-mbp.lsm.local>
2017-07-05 10:34:22 +0100

    * added the html, body and head sections

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
-<?php
-// Drop header in
-require('views/header.php');
+<html>
+  <head>
+    <link rel="stylesheet" href="assets/styles.css" />
+    <script type="text/javascript" src="assets/scripts.js"></script>
+    <title>PHP Unit Testing</title>
+  </head>
 
-$pageList = array('home', 'about', 'contact');
+  <body>
+    <?php
+    // Drop header in
+    require('views/header.php');
 
-// Decide which view to serve
-if(in_array($_GET['page'], $pageList)){
-  $thisPage = $_GET['page'];
-} else {
-  if($_GET['page'] == ''){
-    $thisPage = 'home';
-  } else {
-    $thisPage = '404';
-  }
-}
+    $pageList = array('home', 'about', 'contact');
 
-// Drop main view in
-require('pages/' . $thisPage . '.php');
+    // Decide which view to serve
+    if(in_array($_GET['page'], $pageList)){
+      $thisPage = $_GET['page'];
+    } else {
+      if($_GET['page'] == ''){
+        $thisPage = 'home';
+      } else {
+        $thisPage = '404';
+      }
+    }
 
-// Drop footer in
-require('views/footer.php');
-?>
+    // Drop main view in
+    require('pages/' . $thisPage . '.php');
+
+    // Drop footer in
+    require('views/footer.php');
+    ?>
+  </body>
+</html>
