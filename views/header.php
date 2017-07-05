71c92e4857627b333500c82ae7fc25adb4d5fadf
Jock Perkins <jockperkins@jocks-mbp.lsm.local>
2017-07-05 10:30:55 +0100

    * various page and style updates

--- views/header.php
+++ views/header.php
@@ -1,10 +1,12 @@
 <div id="header">
-  <div id="logo">
-    <img src="http://www.placehold.it/100x50" />
+  <div class="container">
+    <div id="logo">
+      <img src="http://www.placehold.it/100x50" />
+    </div>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/?page=about">About</a></li>
+      <li><a href="/?page=contact">Contact</a></li>
+    </ul>
   </div>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/?page=about">About</a></li>
-    <li><a href="/?page=contact">Contact</a></li>
-  </ul>
 </div>
