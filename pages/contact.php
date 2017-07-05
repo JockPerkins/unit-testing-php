afba95b1c3882628188c37d6a62eb2079fb37fb7
Jock Perkins <jockperkins@jocks-mbp.lsm.local>
2017-07-05 11:06:50 +0100

    * added various markup objects to the pages

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,24 @@
-<h1>Contact page</h1>
+<div id="contact">
+  <h1>Contact page</h1>
+  <form>
+    <div class="form-left">
+      <div class="form-container">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+      </div>
+      <div class="form-container">
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+      </div>
+      <div class="form-container">
+        <label for="telephone">Telephone</label>
+        <input type="text" id="telephone" name="telephone" />
+      </div>
+    </div>
+    <div class="form-right">
+      <div class="form-container">
+        <label for="message">Message</label>
+        <textarea name="message" id="message"></textarea>
+      </div>
+    </div>
+</div>
